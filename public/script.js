7ed54b5086c85daa502a5067b2a72d349125fb25
joaquingomez3 <gomezgonzalod46@gmail.com>
2024-09-26 12:21:30 -0300

    mostrar error de pagina

--- public/script.js
+++ public/script.js
@@ -79,7 +79,7 @@ document.getElementById('searchForm').addEventListener('submit', async function 
 
     } catch (error) {
         console.error('Error al obtener las obras:', error);
-        resultsDiv.innerHTML = '<p>Error al obtener los datos.</p>';
+        resultsDiv.innerHTML = '<p>Error, vercel cancela la consulta si demora un cierto tiempo(EL LOCALHOST ANDA PERFECTO)</p>';
     } finally {
         button.disabled = false;
         document.getElementById('resultsTitle').textContent = 'Resultados:';
